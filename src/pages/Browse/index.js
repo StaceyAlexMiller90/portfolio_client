6a9c1b906e9bfe14c2a10a2461aeb1dba01e02a4
StaceyAlexMiller90 <staceyalexmiller@gmail.com>
2020-04-19 23:32:03 +0200

    Displaying records & added functionality to remove record using icon

--- src/pages/Browse/index.js
+++ src/pages/Browse/index.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { fetchUserRecords } from '../../store/record/actions'
+import { useDispatch, useSelector } from "react-redux";
+import { selectAllUserRecords } from '../../store/record/selectors'
+import Loading from '../../components/Loading'
+import RecordCard from '../../components/RecordCard';
+import '../../components/RecordCard/RecordCard.css'
 
 const Browse = () => {
+  const dispatch = useDispatch()
+  const records = useSelector(selectAllUserRecords)
+
+  useEffect(() => {
+    dispatch(fetchUserRecords())
+  }, [])
+
+  if(!records) {
+    return <Loading />
+  }
+
   return (
-    <div>
-      Browse
+    <div className='body'>
+      {records.length === 0 ? <p>You currently have no records on your shelf</p>
+      : <div className='record-container'> 
+        {records.map(record => {
+        return <RecordCard 
+                key={record.id}
+                id={record.id}
+                title={record.title}
+                artist={record.artist}
+                year={record.year}
+                genre={record.genre}
+                style={record.style}
+                format={record.format}
+                lowestPrice={record.lowestPrice}
+                imageUrl={record.imageUrl}
+              />
+      })}
+      </div>}
     </div>
   )
 }
---
+++ src/store/record/selectors.js
@@ -0,0 +1,1 @@
+export const selectAllUserRecords = state => state.record
