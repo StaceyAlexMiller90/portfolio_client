94f611a96a4f97b2abf372ff01fa6faae35a4c25
StaceyAlexMiller90 <staceyalexmiller@gmail.com>
2020-04-20 20:36:35 +0200

    Adding filtering to records

--- src/pages/Browse/index.js
+++ src/pages/Browse/index.js
@@ -1,43 +1,101 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { fetchUserRecords } from '../../store/record/actions'
 import { useDispatch, useSelector } from "react-redux";
 import { selectAllUserRecords } from '../../store/record/selectors'
 import Loading from '../../components/Loading'
 import RecordCard from '../../components/RecordCard';
 import '../../components/RecordCard/RecordCard.css'
+import MultipleSelect from '../../components/MultipleSelect'
 
 const Browse = () => {
   const dispatch = useDispatch()
   const records = useSelector(selectAllUserRecords)
+  const [genre, setGenre ] = useState([]) 
+  const [style, setStyle ] = useState([]) 
+  const [year, setYear ] = useState([]) 
+  const [artist, setArtist ] = useState([]) 
+
+  const getSelectOptions = (option) => {
+    const options = records.flatMap(record => {
+      if(record[option].length > 1){
+        return record[option].split(' | ')
+      } else {
+        return record[option]
+      }
+    })
+    return [...new Set(options)]
+  }
+
+  const genreArray = getSelectOptions('genre')
+  const styleArray = getSelectOptions('style')
+  const yearArray = getSelectOptions('year')
+  const artistArray = getSelectOptions('artist')
 
   useEffect(() => {
     dispatch(fetchUserRecords())
   }, [])
 
+  const updateFilter = (key, selection) => {
+    if(key === 'genre') {
+      setGenre(selection)
+    } if(key === 'style') {
+      setStyle(selection)
+    } if(key === 'year') {
+      setYear(selection) 
+    } if(key === 'artist') {
+      setArtist(selection)
+    } 
+  }
+
+  const clearFilters = () => {
+    setGenre([])
+    setStyle([])
+    setYear([])
+    setArtist([])
+  }
+
+  const filteredRecords = records.filter(record => {
+    if( (genre.some(item => record.genre.includes(item)) || genre.length === 0) 
+    && (style.some(item => record.style.includes(item)) || style.length === 0)
+    && (year.some(item => record.year === parseInt(item)) || year.length === 0)
+    && (artist.some(item => record.artist.includes(item)) || artist.length === 0) ) {
+      return true
+    } return false
+  })
+ 
   if(!records) {
     return <Loading />
   }
 
   return (
-    <div className='body'>
-      {records.length === 0 ? <p>You currently have no records on your shelf</p>
-      : <div className='record-container'> 
-        {records.map(record => {
-        return <RecordCard 
-                key={record.id}
-                id={record.id}
-                title={record.title}
-                artist={record.artist}
-                year={record.year}
-                genre={record.genre}
-                style={record.style}
-                format={record.format}
-                lowestPrice={record.lowestPrice}
-                imageUrl={record.imageUrl}
-              />
-      })}
-      </div>}
-    </div>
+    <>
+      <div style={{display: 'inline'}}>
+        <MultipleSelect options={genreArray} label={'genre'} updateFilter={updateFilter}/>
+        <MultipleSelect options={styleArray} label={'style'} updateFilter={updateFilter}/>
+        <MultipleSelect options={yearArray} label={'year'} updateFilter={updateFilter}/>
+        <MultipleSelect options={artistArray} label={'artist'} updateFilter={updateFilter}/>
+      </div>
+      <div className='body'>
+        {records.length === 0 ? <p>You currently have no records on your shelf</p>
+        : filteredRecords.length === 0 ? <p> No records match your filters </p>
+        : <div className='record-container'> 
+          {filteredRecords.map(record => {
+          return <RecordCard 
+                  key={record.id}
+                  id={record.id}
+                  title={record.title}
+                  artist={record.artist}
+                  year={record.year}
+                  genre={record.genre}
+                  style={record.style}
+                  format={record.format}
+                  lowestPrice={record.lowestPrice}
+                  imageUrl={record.imageUrl}
+                />
+          })}
+          </div>}
+      </div>
+  </>
   )
 }
 
