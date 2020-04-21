c66f1848900eff9b058a605fb6e951c49daba6f6
StaceyAlexMiller90 <staceyalexmiller@gmail.com>
2020-04-21 18:06:03 +0200

    Updating Card styling

--- src/pages/AddRecords/index.js
+++ src/pages/AddRecords/index.js
@@ -1,25 +1,33 @@
 import React, { useState, useEffect } from 'react'
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from 'react-redux'
 import { selectToken } from '../../store/user/selectors'
-import { useHistory } from "react-router-dom";
+import { useHistory } from 'react-router-dom'
 import AddRecord from '../../components/AddRecord'
 
 const AddRecords = () => {
-  const token = useSelector(selectToken)
-  const history = useHistory()
+	const token = useSelector(selectToken)
+	const history = useHistory()
 
-  useEffect(() => {
-    if (!token) {
-      history.push("/");
-    }
-  }, [token, history]);
-  
-  return (
-    <div>
-      Add a record page
-      <AddRecord />
-    </div>
-  )
+	useEffect(() => {
+		if (!token) {
+			history.push('/')
+		}
+	}, [token, history])
+
+	return (
+		<div>
+			<h3>Add records to your collection!</h3>
+			<h5>Take a quick pic & we do the hard work for you!</h5>
+			<p>
+				This feature uses image recognition technology - once uploaded, your
+				image will be scanned for text & matching articles on the web & the
+				closest matching result is returned.
+			</p>
+			! Please ensure that there is no additional text in your image/background
+			e.g. promotional stickers
+			<AddRecord />
+		</div>
+	)
 }
 
 export default AddRecords
--- src/pages/SearchResults/index.js
+++ src/pages/SearchResults/index.js
@@ -1,55 +1,63 @@
 import React, { useState, useEffect } from 'react'
-import { Container } from "react-bootstrap";
+import Container from '@material-ui/core/Container'
 import Loading from '../../components/Loading'
-import { useDispatch, useSelector } from "react-redux";
-import SuggestionCard from '../../components/SuggestionCard'
+import { useDispatch, useSelector } from 'react-redux'
+import RecordCard from '../../components/RecordCard'
 import { selectToken } from '../../store/user/selectors'
-import { useHistory } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { useHistory } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const SearchResults = (props) => {
-  const token = useSelector(selectToken)
-  const history = useHistory()
-  const keywords = props.suggestions.suggestion
-  const results = props.suggestions.data
+	const token = useSelector(selectToken)
+	const history = useHistory()
+	const keywords = props.suggestions.suggestion
+	const results = props.suggestions.data
 
-  if(!props.suggestions) {
-    return <Loading />
-  } 
+	if (!props.suggestions) {
+		return <Loading />
+	}
 
-  return (
-    <div>
-      <h4>We detect your record as "{keywords}"</h4>
-      <p>Your Image:</p>
-      <img alt='User Upload' style={{width: '100px'}}
-           src={props.recordUrl}></img>
-       <Link to='/manualadd'>  
-        <button>Click to add manually</button>
-      </Link>  
-      <Link to='/addrecord'>
-        <button>Click here to retake picture</button>
-      </Link>
-      <p>Search results:</p>
-      <Container className='mb-4 m-auto row d-flex align-items-stretch'>
-      {results.length === 0 ? 
-      <p>Sorry, no records were found</p>
-      : results.map(result => {
-        return <SuggestionCard 
-                key={result.id}
-                id={result.id} 
-                title={result.title}
-                artist={result.artist}
-                imageUrl={result.imageUrl}
-                genre={result.genre}
-                style={result.style}
-                year={result.year}
-                lowestPrice={result.lowestPrice}
-                format={result.format}
-                />
-      })}
-       </Container>
-    </div>
-  )
+	return (
+		<div>
+			<h4>We detect your record as "{keywords}"</h4>
+			<p>Your Image:</p>
+			<img
+				alt="User Upload"
+				style={{ width: '100px' }}
+				src={props.recordUrl}
+			></img>
+			<Link to="/manualadd">
+				<button>Click to add manually</button>
+			</Link>
+			<Link to="/addrecord">
+				<button>Click here to retake picture</button>
+			</Link>
+			<p>Search results:</p>
+			<div style={{ marginLeft: '100px' }}>
+				{results.length === 0 ? (
+					<p>Sorry, no records were found</p>
+				) : (
+					results.map((result) => {
+						return (
+							<RecordCard
+								key={result.id}
+								suggestion={true}
+								id={result.id}
+								title={result.title}
+								artist={result.artist}
+								imageUrl={result.imageUrl}
+								genre={result.genre}
+								style={result.style}
+								year={result.year}
+								lowestPrice={result.lowestPrice}
+								format={result.format}
+							/>
+						)
+					})
+				)}
+			</div>
+		</div>
+	)
 }
 
 export default SearchResults
