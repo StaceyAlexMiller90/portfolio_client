e849d0fc2ddc3c33ffedaa50654ebc880090b8d9
StaceyAlexMiller90 <staceyalexmiller@gmail.com>
2020-04-21 12:38:17 +0200

    Adding search bar for record title

--- src/components/MultipleSelect/index.js
+++ src/components/MultipleSelect/index.js
@@ -1,94 +1,94 @@
 import React, { useState } from 'react'
-import { makeStyles, useTheme } from "@material-ui/core/styles";
-import Input from "@material-ui/core/Input";
-import InputLabel from "@material-ui/core/InputLabel";
-import MenuItem from "@material-ui/core/MenuItem";
-import FormControl from "@material-ui/core/FormControl";
-import Select from "@material-ui/core/Select";
-import Chip from "@material-ui/core/Chip";
+import { makeStyles, useTheme } from '@material-ui/core/styles'
+import Input from '@material-ui/core/Input'
+import InputLabel from '@material-ui/core/InputLabel'
+import MenuItem from '@material-ui/core/MenuItem'
+import FormControl from '@material-ui/core/FormControl'
+import Select from '@material-ui/core/Select'
+import Chip from '@material-ui/core/Chip'
 
-const useStyles = makeStyles(theme => ({
-  formControl: {
-    margin: theme.spacing(1),
-    minWidth: 120,
-    maxWidth: 300
-  },
-  chips: {
-    display: "flex",
-    flexWrap: "wrap"
-  },
-  chip: {
-    margin: 2
-  },
-  noLabel: {
-    marginTop: theme.spacing(3)
-  }
-}));
+const useStyles = makeStyles((theme) => ({
+	formControl: {
+		margin: theme.spacing(1),
+		minWidth: 120,
+		maxWidth: 300,
+	},
+	chips: {
+		display: 'flex',
+		flexWrap: 'wrap',
+	},
+	chip: {
+		margin: 2,
+	},
+	noLabel: {
+		marginTop: theme.spacing(3),
+	},
+}))
 
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
+const ITEM_HEIGHT = 48
+const ITEM_PADDING_TOP = 8
 const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250
-    }
-  }
-};
+	PaperProps: {
+		style: {
+			maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+			width: 250,
+		},
+	},
+}
 
 const getStyles = (option, filter, theme) => {
-  return {
-    fontWeight:
-     filter.indexOf(option) === -1
-        ? theme.typography.fontWeightRegular
-        : theme.typography.fontWeightMedium
-  };
+	return {
+		fontWeight:
+			filter.indexOf(option) === -1
+				? theme.typography.fontWeightRegular
+				: theme.typography.fontWeightMedium,
+	}
 }
 
 const MultipleSelect = (props) => {
-  const options = props.options
-  const label = props.label
-  const classes = useStyles();
-  const theme = useTheme();
+	const options = props.options
+	const label = props.label
+	const classes = useStyles()
+	const theme = useTheme()
 
-  const [ filter, setFilter ] = useState([]);
+	const [filter, setFilter] = useState([])
 
-  const handleChange = event => {
-    setFilter(event.target.value)    
-    props.updateFilter(label, event.target.value)
-  };
+	const handleChange = (event) => {
+		setFilter(event.target.value)
+		props.updateFilter(label, event.target.value)
+	}
 
-  return (
-      <FormControl className={classes.formControl}>
-        <InputLabel id="demo-mutiple-chip-label">{label}</InputLabel>
-        <Select
-          labelId="demo-mutiple-chip-label"
-          id="demo-mutiple-chip"
-          multiple
-          value={filter}
-          onChange={handleChange}
-          input={<Input id="select-multiple-chip" />}
-          renderValue={selected => (
-            <div className={classes.chips}>
-              {selected.map(value => (
-                <Chip key={value} label={value} className={classes.chip} />
-              ))}
-            </div>
-          )}
-          MenuProps={MenuProps}
-        >
-          {options.map(option => (
-            <MenuItem
-              key={option}
-              value={option}
-              style={getStyles(option, filter, theme)}
-            >
-              {option}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-  );
+	return (
+		<FormControl className={classes.formControl}>
+			<InputLabel id="demo-mutiple-chip-label">{label}</InputLabel>
+			<Select
+				labelId="demo-mutiple-chip-label"
+				id="demo-mutiple-chip"
+				multiple
+				value={filter}
+				onChange={handleChange}
+				input={<Input id="select-multiple-chip" />}
+				renderValue={(selected) => (
+					<div className={classes.chips}>
+						{selected.map((value) => (
+							<Chip key={value} label={value} className={classes.chip} />
+						))}
+					</div>
+				)}
+				MenuProps={MenuProps}
+			>
+				{options.map((option) => (
+					<MenuItem
+						key={option}
+						value={option}
+						style={getStyles(option, filter, theme)}
+					>
+						{option}
+					</MenuItem>
+				))}
+			</Select>
+		</FormControl>
+	)
 }
 
 export default MultipleSelect
